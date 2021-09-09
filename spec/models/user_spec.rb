07e12d94f9b355cf26262404fe1cb179f9edf331
Raymond <raymond@philippinedev.com>
2021-09-09 22:01:05 +0800

    create test for users email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email) { Faker::Internet.email }
+
+  describe '#email' do
+    context 'when empty' do
+      let(:email) { '' }
+
+      it 'will error' do
+        expect(email).to be_empty
+      end
+    end
+
+    context 'when valid' do
+      it 'will succeed' do
+        expect(email).to_not be_empty
+      end
+    end
+  end
 end
